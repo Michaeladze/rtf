b8a8134f39dcbd666c9fa06fcf2b9e479da0a32b
Author <Author@author.com>
2019-12-25 16:46:16 +0300

    asdf

---
+++ RTF/Controller/UserProfilePopupController.swift
@@ -0,0 +1,20 @@
+//
+//  userProfilePopup.swift
+//  RTF
+//
+//  Created by 16688500 on 22.12.2019.
+//  Copyright © 2019 16688500. All rights reserved.
+//
+
+import UIKit
+
+
+class UserProfilePopupController: UIViewController {
+    @IBAction func closePopup(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func closePopupTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
